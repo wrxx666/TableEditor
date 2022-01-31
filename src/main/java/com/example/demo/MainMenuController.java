2b0b7f22dd8ebbfd24a2a8068d32c51f9d5c2a40
wrxx666 <maxf74@live.com>
2022-01-31 22:17:40 +0500

    Upload0.3

---
+++ src/main/java/com/example/demo/MainMenuController.java
@@ -0,0 +1,92 @@
+package com.example.demo;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.FocusModel;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import static javafx.scene.input.KeyCode.R;
+
+
+public class MainMenuController {
+    @FXML
+    private ResourceBundle resources;
+
+    private HashMap<String,String> filesOfTableList;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button deleteTableBtn;
+
+    @FXML
+    private Button dwnldTableBtn;
+
+    @FXML
+    private ListView tablesList;
+
+    @FXML
+    private Button exitBtn;
+
+    @FXML
+    private Button loadTableBtn;
+
+    @FXML
+    private ImageView tableMapView;
+
+    @FXML
+    void initialize() {
+        assert deleteTableBtn != null : "fx:id=\"deleteTableBtn\" was not injected: check your FXML file 'mainMenu.fxml'.";
+        assert dwnldTableBtn != null : "fx:id=\"dwnldTableBtn\" was not injected: check your FXML file 'mainMenu.fxml'.";
+        assert exitBtn != null : "fx:id=\"exitBtn\" was not injected: check your FXML file 'mainMenu.fxml'.";
+        assert loadTableBtn != null : "fx:id=\"loadTableBtn\" was not injected: check your FXML file 'mainMenu.fxml'.";
+        assert tableMapView != null : "fx:id=\"tableMapView\" was not injected: check your FXML file 'mainMenu.fxml'.";
+        ObservableList<String> tables = FXCollections.observableArrayList("First Floor", "Second Floor", "Summer terrace", "Roof");
+        tablesList.setItems(tables);
+        filesOfTableList = new HashMap<>();
+        filesOfTableList.put("First Floor", "./src/main/resources/com/example/demo/firstFloor.png");
+        filesOfTableList.put("Second Floor", "./src/main/resources/com/example/demo/tableEditor.png");
+        filesOfTableList.put("Summer terrace", "./src/main/resources/com/example/demo/tableEditor.png");
+        filesOfTableList.put("Roof", "./src/main/resources/com/example/demo/tableEditor.png");
+
+    }
+
+    public void onExitBtnClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(TableEditorApplication.class.getResource("signin.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 250);
+        Stage pr = (Stage) exitBtn.getScene().getWindow();
+        pr.setScene(scene);
+    }
+    public void onLoadTableBtnClick() throws MalformedURLException {
+        tablesList.refresh();
+        String s = tablesList.getSelectionModel().getSelectedItems().toString();
+        StringBuilder sb = new StringBuilder(s);
+        sb.deleteCharAt(0);
+        sb.deleteCharAt(sb.length()-1);
+        s = sb.toString();
+        System.out.println(s);
+        System.out.println(filesOfTableList.get(s));
+        Path imageFile = Paths.get(filesOfTableList.get(s));
+        tableMapView.setImage(new Image(imageFile.toUri().toURL().toExternalForm()));
+    }
+}
--- src/main/java/com/example/demo/SignInController.java
+++ src/main/java/com/example/demo/SignInController.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignInController {
 
@@ -23,8 +28,12 @@ public class SignInController {
         @FXML
         private ImageView tableEditorImage;
 
-        public void onSignInButtonClick(){
+        public void onSignInButtonClick() throws IOException {
                 loginField.appendText("LOLOLOLOLOLOL");
+                FXMLLoader fxmlLoader = new FXMLLoader(TableEditorApplication.class.getResource("mainMenu.fxml"));
+                Scene scene = new Scene(fxmlLoader.load(), 876, 451);
+                Stage pr = (Stage) signInButton.getScene().getWindow();
+                pr.setScene(scene);
         }
 
 }
