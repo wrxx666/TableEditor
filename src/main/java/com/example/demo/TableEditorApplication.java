603ea30e91cfbc81bb99e99f7d7ddcdf49b14604
wrxx666 <maxf74@live.com>
2022-01-31 18:54:19 +0500

    Upload0.1

--- src/main/java/com/example/demo/TableEditorApplication.java
+++ src/main/java/com/example/demo/TableEditorApplication.java
@@ -3,20 +3,21 @@ package com.example.demo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TableEditorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signin.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TableEditorApplication.class.getResource("signin.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 250);
 
         stage.setTitle("TableEditor");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
